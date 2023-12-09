c27d6e5befcd0a49ef87a920a3720b595b938212
Jacob Scheiffler <jacob.scheiffler@colorado.edu>
2023-12-08 17:24:38 -0700

    Initial savings calculator commit

---
+++ SavingsCalculator/savings_calculator.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+from datetime import date
+
+def get_days_until(target_date):
+    # Define tuple containing the day in the 
+    # year at the start of each month
+    dpm = (0, 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334)
+
+    # Get target day, month and year
+    target_date = target_date.split('/')
+    target_month = int(target_date[0])
+    target_day = int(target_date[1])
+    target_year = int(target_date[2])
+    target_diy = dpm[target_month] + target_day     #Compute day in year
+
+    # Get current day, month and, year
+    current_date = str(date.today()).split('-')
+    current_year = int(current_date[0])
+    current_month = int(current_date[1])
+    current_day = int(current_date[2])
+    current_diy = dpm[current_month] + current_day  #Compute day in year
+
+    # Figure out the number of days between current and target dates
+    if current_year > target_year:
+        # Target year is previous year
+        nd = -1
+    elif current_year == target_year:
+        # Target year is this year
+        if current_diy > target_diy:
+            # Date already passed
+            nd = -1
+        else:
+            nd = target_diy - current_diy
+    else:
+        # Target year is in future years
+        ny = target_year - current_year - 1     #Years between
+
+        # Days til end of year + years between + target day in year
+        nd = (365 - current_diy) + (ny * 365) + target_diy
+
+    return nd
+
+
+if __name__ == '__main__':
+    # Set your savings goal
+    goal = int(input("Enter your savings goal: "))
+
+    # Set savings goal target date
+    target_date = input("Enter your target date (MM/DD/YYYY): ")
+
+    # Get interest rate of savings account
+    ir = float(input("Enter the APY of your savings account (XX.XX %): ")) / 100
+    ig = (28 * ir * 0.75) / 365  # monthly inerest gained factor
+
+    # Get current amount in savings
+    p = int(input("Enter the current amount you have in savings: "))
+
+    nd = get_days_until(target_date)
+    print('Days between: ' + str(nd))
+
+
+    
